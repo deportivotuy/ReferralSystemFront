1ebdef11b93a4760bb60b8c8722810841a45c19e
rjsimosa222 <rjsimosa@gmail.com>
2024-11-01 23:21:40 -0500

    Update setUrl.js

--- src/config/setUrl.js
+++ src/config/setUrl.js
@@ -1,4 +1,5 @@
 export function setUrl() {
     const API_URL = 'http://localhost:3000/api';
+    //const API_URL = 'https://api-referral-system-gac0fzc0gbfeccgz.northeurope-01.azurewebsites.net/api';
     return API_URL;
 }
